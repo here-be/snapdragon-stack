daa54ebaf73bacccd643f6f8d9be383448d6ad83
jonschlinkert <jon.schlinkert@sellside.com>
2018-01-08 01:18:21 -0500

    add methods

--- index.js
+++ index.js
@@ -5,15 +5,35 @@
  */
 
 module.exports = class Stack extends Array {
-  get prev() {
+  get first() {
+    return this[0];
+  }
+
+  get last() {
     return this[this.length - 1];
   }
 
+  get current() {
+    return this.last;
+  }
+
+  get prev() {
+    return this[this.length - 2];
+  }
+
   get firstChild() {
-    return this.prev ? this.prev.firstChild : null;
+    if (isObject(this.first) && Array.isArray(this.first.nodes)) {
+      return this.first.nodes[0];
+    }
   }
 
   get lastChild() {
-    return this.prev ? this.prev.lastChild : null;
+    if (isObject(this.last) && Array.isArray(this.last.nodes)) {
+      return this.last.nodes[this.last.nodes.length - 1];
+    }
   }
 };
+
+function isObject(val) {
+  return val && typeof val === 'object';
+}
